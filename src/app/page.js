1a06f4f73d42a48190b2409dcf6dfc6768cb3666
Jonas Holbech <jh@jonasholbech.dk>
2023-09-04 10:52:47 +0200

    title

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default async function Home() {
   let { data, error } = await getRooms();
   return (
     <main>
+      <h1>MMD Køen, V2</h1>
       <RoomPicker rooms={data} />
     </main>
   );
