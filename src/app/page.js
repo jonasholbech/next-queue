825fa00e54cf69dc15f8c071436d8e6992232efa
Jonas Holbech <jh@jonasholbech.dk>
2023-09-13 22:46:44 +0200

    minimal styling index

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import { getRooms } from "@/utils/requests";
 import RoomPicker from "./components/RoomPicker";
-
+import styles from "./page.module.css";
 export default async function Home() {
   let { data, error } = await getRooms();
   return (
-    <main>
+    <main className={styles.main}>
       <h1>MMD Køen, V2</h1>
       <RoomPicker rooms={data} />
     </main>
