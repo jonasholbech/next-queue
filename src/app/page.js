20090d7f026bab7ccd6e60abf8d82d11a2d8a532
Jonas Holbech <jh@jonasholbech.dk>
2023-09-25 12:00:49 +0200

    testing vercel env vars

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
 import { getRooms } from "@/utils/requests";
 import RoomPicker from "./components/RoomPicker";
 import styles from "./page.module.css";
+import { getAbsoluteUrl } from "@/utils/vercel-utils";
 export default async function Home() {
   let { data, error } = await getRooms();
+  const users = await fetch(getAbsoluteUrl() + "/api/swapi").then((res) =>
+    res.json()
+  );
+  console.log(users);
+  console.log(process.env.VERCEL_URL);
   return (
     <main className={styles.main}>
       <h1>MMD Køen, V2</h1>
---
+++ src/utils/vercel-utils.js
@@ -0,0 +1,15 @@
+export const IS_SERVER = typeof window === "undefined";
+export const IS_DEV = process.env.NODE_ENV === "development";
+/* export function getProtocol() {
+  const isProd = process.env.VERCEL_ENV === "production";
+  if (isProd) return "https://";
+  return "http://";
+} */
+export function getAbsoluteUrl() {
+  //get absolute url in client/browser
+  if (IS_DEV) {
+    return `http://localhost:3000`;
+  } else {
+    return process.env.VERCEL_URL;
+  }
+}
