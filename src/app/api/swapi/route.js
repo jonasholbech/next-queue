952cb17961725e7cf3392be453958eb171281e49
Jonas Holbech <jh@jonasholbech.dk>
2023-09-25 12:13:01 +0200

    redeploy, please

--- src/app/api/swapi/route.js
+++ src/app/api/swapi/route.js
@@ -1,11 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  const res = await fetch("https://swapi.dev/api/people/1/", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export async function GET() {
+  const res = await fetch("https://swapi.dev/api/people/1/");
   const data = await res.json();
 
   return NextResponse.json({ data });
