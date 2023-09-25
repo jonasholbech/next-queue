9ede2aee7119083c5e69a1516e427cf39a0954f7
Jonas Holbech <jh@jonasholbech.dk>
2023-09-25 16:00:27 +0200

    F#€K NU AF

--- src/utils/vercel-utils.js
+++ src/utils/vercel-utils.js
@@ -2,10 +2,9 @@ export const IS_SERVER = typeof window === "undefined";
 export const IS_LOCAL = process.env.NODE_ENV === "production" ? false : true;
 
 export function getAbsoluteUrl() {
-  //get absolute url in client/browser
   if (IS_LOCAL) {
     return `http://localhost:3000`;
   } else {
-    return `https://${process.env.VERCEL_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 }
